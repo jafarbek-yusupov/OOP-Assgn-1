3f9a83e5d647507a027d35b9930d77a8d54c67b9
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-02-06 21:16:01 +0500

    ILDMLFY

--- LMS.cpp
+++ LMS.cpp
@@ -10,14 +10,14 @@ void LMS::addStudent(Student student){
 }
 
 void LMS::addCourse(Course course){
-    for(const auto& c : courses){ if(c.getId() == course.getId()){ cout << "Course with ID " << course.getId() << " ALR EXISTSSSSSSSSS!!!\n"; /*check for dups*/ return;}}  courses.push_back(course);
+    for(const auto& c : courses){ if(c.getId() == course.getId()){ cout << "Course with ID " << course.getId() << " ALR EXISTSSSSSSSSS!!!\n"; /*check for dups*/ return;}} courses.push_back(course);
 }
 
 void LMS::addStudentToCourse(int studentId, const string& courseId){
-    auto courseIt = find_if(courses.begin(), courses.end(), [&courseId](const Course& c){ return c.getId() == courseId;}); // find crs by id
+    auto courseIt = std::find_if(courses.begin(), courses.end(), [=](const Course& c) { return c.getId() == courseId; }); // find crs by id
     if(courseIt == courses.end()){ cout << "Course with ID " << courseId << " not found\n"; return;}
 
-    auto studentIt = find_if(students.begin(), students.end(), [studentId](const Student& s){ return s.getId() == studentId;}); // find stud by id
+    auto studentIt = std::find_if(students.begin(), students.end(), [=](const Student& s) { return s.getId() == studentId; }); // find stud by id
     if(studentIt == students.end()){ cout << "Student with ID " << studentId << " not found\n"; return;}
 //cout << "Student " << studentIt->getName() << " added to course " << courseIt->getName() << ".\n";
     if(!(courseIt->addStudent(*studentIt))){ cout << "Student " << studentIt->getName() << " is already enrolled in course " << courseIt->getName() << "!!!\n";} 
@@ -25,20 +25,14 @@ void LMS::addStudentToCourse(int studentId, const string& courseId){
 
 void LMS::printDetails() const{
     cout << "LMS Name: " << name << "\nStudents:\n";
-    for (const auto& student : students){
+    for(const auto& student : students){
         cout << "Student ID: " << student.getId() << ", Name: " << student.getName() << "\nEnrolled Courses: ";
-        for(const auto& course : courses){
-            if(course.hasStudent(student)){ cout << course.getId() << " ";}
-        }
-    cout << endl;
+        for(const auto& course : courses){ if(course.hasStudent(student)){ cout << course.getId() << " ";}} cout << "\n";
     }
 
     cout << "Courses:\n";
-    for (const auto& course : courses){
+    for(const auto& course : courses){
         cout << "Course ID: " << course.getId() << ", Name: " << course.getName() << ", Credits: " << course.getCredits() << "\nEnrolled Students:";
-        for(const auto& student : students){ 
-      if(course.hasStudent(student)){ cout << " " << student.getId();}
-        }
-        cout << endl;
+        for(const auto& student : students){ if(course.hasStudent(student)){ cout << " " << student.getId();}} cout << "\n";
     }
 }
