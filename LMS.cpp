a0b0f7c694ef12b93f825e70c62c7644da2ade7c
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-02-06 21:25:48 +0500

    /_-_\

--- LMS.cpp
+++ LMS.cpp
@@ -10,14 +10,14 @@ void LMS::addStudent(Student student){
 }
 
 void LMS::addCourse(Course course){
-    for(const auto& c : courses){ if(c.getId() == course.getId()){ cout << "Course with ID " << course.getId() << " ALR EXISTSSSSSSSSS!!!\n"; /*check for dups*/ return;}} courses.push_back(course);
+    for(const auto& c : courses){ if(c.getId() == course.getId()){ cout << "Course with ID " << course.getId() << " ALR EXISTSSSSSSSSS!!!\n"; /*check for dups*/ return;}}  courses.push_back(course);
 }
 
 void LMS::addStudentToCourse(int studentId, const string& courseId){
-    auto courseIt = std::find_if(courses.begin(), courses.end(), [=](const Course& c) { return c.getId() == courseId; }); // find crs by id
+    auto courseIt = std::find_if(courses.begin(), courses.end(), [&](const Course& c) { return c.getId() == courseId; }); // find crs by id
     if(courseIt == courses.end()){ cout << "Course with ID " << courseId << " not found\n"; return;}
 
-    auto studentIt = std::find_if(students.begin(), students.end(), [=](const Student& s) { return s.getId() == studentId; }); // find stud by id
+    auto studentIt = std::find_if(students.begin(), students.end(), [&](const Student& s) { return s.getId() == studentId; }); // find stud by id
     if(studentIt == students.end()){ cout << "Student with ID " << studentId << " not found\n"; return;}
 //cout << "Student " << studentIt->getName() << " added to course " << courseIt->getName() << ".\n";
     if(!(courseIt->addStudent(*studentIt))){ cout << "Student " << studentIt->getName() << " is already enrolled in course " << courseIt->getName() << "!!!\n";} 
