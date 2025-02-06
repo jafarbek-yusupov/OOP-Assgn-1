21805cfb80a3d554c201c2366f007e6e21958072
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-02-06 22:32:46 +0500

    kkkkkkkkkkkkk

--- LMS.h
+++ LMS.h
@@ -6,13 +6,13 @@
 #include "Course.h"
 
 class LMS{
-private: std::string name; std::vector<Student> students; std::vector<Course> courses;
-public:
-LMS(const std::string& name);
-void addStudent(Student student);
-void addCourse(Course course);
-void addStudentToCourse(int studentId, int courseId); //void addStudentToCourse(Student student, Course course);
-void printDetails() const;
+	private: std::string name; std::vector<Student> students; std::vector<Course> courses;
+	public:
+		LMS(const std::string& name);
+		void addStudent(Student student);
+		void addCourse(Course course);
+		void addStudentToCourse(int studentId, int courseId); //void addStudentToCourse(Student student, Course course);
+		void printDetails() const;
 };
 
 #endif
