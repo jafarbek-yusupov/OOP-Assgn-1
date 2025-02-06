02d4c1a44ba57bb67e04f3e2dd553d11d498d0a2
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-02-06 20:55:33 +0500

    aaa

--- LMS.cpp
+++ LMS.cpp
@@ -15,10 +15,10 @@ void LMS::addCourse(Course course){
 
 void LMS::addStudentToCourse(int studentId, const string& courseId){
     auto courseIt = find_if(courses.begin(), courses.end(), [&courseId](const Course& c){ return c.getId() == courseId;}); // find crs by id
-    if(courseIt == courses.end()){ cout << "Course with ID " << courseId << " not found!!!!\n"; return;}
+    if(courseIt == courses.end()){ cout << "Course with ID " << courseId << " not found\n"; return;}
 
     auto studentIt = find_if(students.begin(), students.end(), [studentId](const Student& s){ return s.getId() == studentId;}); // find stud by id
-    if(studentIt == students.end()){ cout << "Student with ID " << studentId << " not found!!!!\n"; return;}
+    if(studentIt == students.end()){ cout << "Student with ID " << studentId << " not found\n"; return;}
 //cout << "Student " << studentIt->getName() << " added to course " << courseIt->getName() << ".\n";
     if(!(courseIt->addStudent(*studentIt))){ cout << "Student " << studentIt->getName() << " is already enrolled in course " << courseIt->getName() << "!!!\n";} 
 }
