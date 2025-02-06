4f365917d68384fb6dc499dc280159dda4e89fbf
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-02-06 14:11:29 +0500

    letssee

--- LMS.cpp
+++ LMS.cpp
@@ -3,14 +3,16 @@
 #include <algorithm>
 using namespace std;
 
-LMS::LMS(const string& name) : name(name) {}
+LMS::LMS(const string& name) : name(name){}
 
-void LMS::addStudent(Student student) {
-    for(const auto& s : students){ if(s.getId() == student.getId()){ cout << "Student with ID " << student.getId() << " already exists!!!\n"; /*check for dups*/ return;}} students.push_back(student);
+void LMS::addStudent(Student student){
+//    for(const auto& s : students){ if(s.getId() == student.getId()){ cout << "Student with ID " << student.getId() << " ALR EXISTSSSSSSSSS!!!\n"; /*check for dups*/ return;}} 
+	students.push_back(student);
 }
 
-void LMS::addCourse(Course course) {
-    for(const auto& c : courses){ if(c.getId() == course.getId()){ cout << "Course with ID " << course.getId() << " already exists!!!\n"; /*check for dups*/ return;}} courses.push_back(course);
+void LMS::addCourse(Course course){
+//    for(const auto& c : courses){ if(c.getId() == course.getId()){ cout << "Course with ID " << course.getId() << " ALR EXISTSSSSSSSSS!!!\n"; /*check for dups*/ return;}} 
+	courses.push_back(course);
 }
 
 void LMS::addStudentToCourse(int studentId, const string& courseId){
@@ -19,7 +21,26 @@ void LMS::addStudentToCourse(int studentId, const string& courseId){
 
     auto studentIt = find_if(students.begin(), students.end(), [studentId](const Student& s){ return s.getId() == studentId;}); // find stud by id
     if(studentIt == students.end()){ cout << "Student with ID " << studentId << " not found!!!!\n"; return;}
+//cout << "Student " << studentIt->getName() << " added to course " << courseIt->getName() << ".\n";
+    if(!(courseIt->addStudent(*studentIt))){ cout << "Student " << studentIt->getName() << " is already enrolled in course " << courseIt->getName() << "!!!\n";} 
+}
 
-    if(courseIt->addStudent(*studentIt)){ cout << "Student " << studentIt->getName() << " added to course " << courseIt->getName() << ".\n";} 
-	else{ cout << "Student " << studentIt->getName() << " is already enrolled in course " << courseIt->getName() << ".\n";}
+void LMS::printDetails() const{
+    cout << "LMS Name: " << name << "\nStudents:\n";
+    for (const auto& student : students){
+        cout << "Student ID: " << student.getId() << ", Name: " << student.getName() << "\nEnrolled Courses: ";
+        for(const auto& course : courses){
+            if(course.hasStudent(student)){ cout << course.getId() << " ";}
+        }
+		cout << endl;
+    }
+
+    cout << "Courses:" << endl;
+    for (const auto& course : courses) {
+        cout << "Course ID: " << course.getId() << ", Name: " << course.getName() << ", Credits: " << course.getCredits() << "\nEnrolled Students: ";
+        for(const auto& student : students){ 
+			if(course.hasStudent(student)){ cout << student.getId() << " ";}
+        }
+        cout << endl;
+    }
 }
