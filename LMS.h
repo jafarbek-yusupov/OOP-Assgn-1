2bd8ce4646ce53297b07e947baca613630a99ed5
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-02-06 22:34:34 +0500

    aaaaa

--- LMS.h
+++ LMS.h
@@ -8,11 +8,11 @@
 class LMS{
 private: std::string name; std::vector<Student> students; std::vector<Course> courses;
 public:
-	LMS(const std::string& name);
-	void addStudent(Student student);
-	void addCourse(Course course);
-	void addStudentToCourse(int studentId, int courseId); //void addStudentToCourse(Student student, Course course);
-	void printDetails() const;
+  LMS(const std::string& name);
+  void addStudent(Student student);
+  void addCourse(Course course);
+  void addStudentToCourse(int studentId, int courseId); //void addStudentToCourse(Student student, Course course);
+  void printDetails() const;
 };
 
 #endif
