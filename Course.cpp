80ca87ab92e95b6a36b5e0ee944db34ac812bc64
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-02-07 13:29:10 +0500

    vv

--- Course.cpp
+++ Course.cpp
@@ -2,12 +2,11 @@
 #include "Student.cpp"
 #include <algorithm>
 #include <iostream>
-using namespace std;
 
-Course::Course(int id, const string& name, int credits) : id(id), name(name), credits(credits) {}
+Course::Course(int id, const std::string& name, int credits) : id(id), name(name), credits(credits) {}
 
 int Course::getId() const{ return id;}
-string Course::getName() const{ return name;}
+std::string Course::getName() const{ return name;}
 int Course::getCredits() const{ return credits;}
 
 bool Course::hasStudent(const Student& student) const{ return find(students.begin(), students.end(), student) != students.end();}
@@ -17,6 +16,6 @@ bool Course::addStudent(Student& student){ if(hasStudent(student)){ return false
 void Course::dropStudent(Student& student){ for(auto ii = students.begin(); ii != students.end(); ii++){ if((ii)->getId() == student.getId()){ students.erase(ii); break;}}}
 
 void Course::printDetails() const{
-    cout << "Course ID: " << id << ", Name: " << name << ", Credits: " << credits << endl << "Enrolled Students:";
-    for(const auto& ss : students){ cout << " " << ss.getId();} cout << endl;
+    std::cout << "Course ID: " << id << ", Name: " << name << ", Credits: " << credits << std::endl << "Enrolled Students:";
+    for(const auto& ss : students){ std::cout << " " << ss.getId();} std::cout << std::endl;
 }
